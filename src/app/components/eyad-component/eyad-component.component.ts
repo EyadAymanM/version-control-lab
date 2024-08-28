51bd5fca27cda017cb31185fcccf9785ea020cad
EyadAymanM <eiadaiman51@gmail.com>
2024-08-28 11:29:22 +0300

    removed extra unneeded routes and added my compenent

---
+++ src/app/components/eyad-component/eyad-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-eyad-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './eyad-component.component.html',
+  styleUrl: './eyad-component.component.scss'
+})
+export class EyadComponentComponent {
+
+}
